586a530ea607e65243eef838f1f9f56e1b1267f0
afrizaharahap <Afriza.harahap@gmail.com>
2023-10-25 11:26:17 +0700

    remove bcrypt

--- src/pages/api/auth/authOptions.js
+++ src/pages/api/auth/authOptions.js
@@ -1,19 +1,9 @@
 import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
-import GithubProvider from 'next-auth/providers/github'
 
 const authURL = process.env.NEXTAUTH_URL
 
 export const authOptions = {
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
-    }),
     CredentialsProvider({
       name: 'credentials',
       credentials: {
